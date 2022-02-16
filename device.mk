8b92c61c8f3ed5f5c778b88417c72c4f953fc9ab
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-16 22:14:58 +0200

    a52sxq: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb

--- device.mk
+++ device.mk
@@ -212,7 +212,9 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.samsung-multihal
+    android.hardware.sensors@2.1-service.samsung-multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    sensors.samsung
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
