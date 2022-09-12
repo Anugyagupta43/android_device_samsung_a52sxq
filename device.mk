6519062fd8ba7bef874d39e7283592c486b7643f
Alone0316 <rutujamandlik06@gmail.com>
2022-09-12 10:16:11 +0000

    a52sxq: Build android.hardware.bluetooth@1.0.so from source 01-08 14:32:40.290   877   877 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti": library "android.hardware.bluetooth@1.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
