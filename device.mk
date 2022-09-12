02450c8dad7932bdb10219d7a229bb1b05a010d7
Alone0316 <rutujamandlik06@gmail.com>
2022-09-12 10:17:46 +0000

    a52sxq: Build libhwbinder.so from source 01-08 14:32:40.294   874   874 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.audio.service": library "libhwbinder.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -53,7 +53,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor
+    android.hidl.manager@1.0.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Health
 PRODUCT_PACKAGES += \
