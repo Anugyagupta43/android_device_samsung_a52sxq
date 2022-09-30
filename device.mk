2cccca7c0bcd57202af87ebf12aeb15bb92933f5
Cosmin Tanislav <demonsingur@gmail.com>
2022-09-30 11:11:00 +0000

    a52sxq: Import hals.conf

--- device.mk
+++ device.mk
@@ -207,6 +207,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
