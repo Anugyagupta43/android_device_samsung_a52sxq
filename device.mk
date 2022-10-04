5acabfca61af7fa59934b9f3d907cb30302825fa
Alone0316 <rutujamandlik06@gmail.com>
2022-10-04 10:36:43 +0000

    try to enable adb

--- device.mk
+++ device.mk
@@ -267,6 +267,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.control_privapp_permissions=log
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.control_privapp_permissions=log \
     ro.secure=0 \
