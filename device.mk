6663f8548fd2f60f3e1c67ddf8e0a3118f0a994c
Arian <arian.kulmer@web.de>
2022-10-15 09:17:35 +0000

    a52sxq: Build camera provider from source

--- device.mk
+++ device.mk
@@ -86,8 +86,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.device@3.6.vendor \
-    android.hardware.camera.provider@2.5-legacy
+    android.hardware.camera.provider@2.6-impl \
+    android.hardware.camera.provider@2.6-service_64
 
 # Cas
 PRODUCT_PACKAGES += \
