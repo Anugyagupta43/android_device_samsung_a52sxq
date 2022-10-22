89029e28c1f0966e53340a4c69f9ce89dc600e01
Alone0316 <rutujamandlik06@gmail.com>
2022-10-22 05:40:23 +0000

    a52sxq: Build vendor.qti.hardware.perf from source

--- device.mk
+++ device.mk
@@ -232,7 +232,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.3 \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
