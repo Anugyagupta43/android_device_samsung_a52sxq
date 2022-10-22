73df155a601deff1240f105fa03b2fa4e67ad5e2
Alone0316 <rutujamandlik06@gmail.com>
2022-10-22 11:33:34 +0000

    a52sxq: Build bluetooth libs from source *fixes 11-11 12:53:17.381   869   869 E LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default. 11-11 12:53:17.382   584   584 I hwservicemanager: getTransport: Cannot find entry android.hardware.bluetooth.audio@2.0::IBluetoothAudioProvidersFactory/default in either framework or device VINTF manifest. 11-11 12:53:17.382   869   869 E LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.audio@2.0::IBluetoothAudioProvidersFactory/default.

--- device.mk
+++ device.mk
@@ -58,6 +58,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor
 
 # Display
